ef03b38f31c28a02ba15ff8ae0dfdf1b1e86f984
Nyarai Nhamoinesu <nyarainhamoinesu@gmail.com>
2020-03-21 18:51:40 -0500

    finalupdates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,54 @@
 // from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+var data = data;
+
+var tbody = d3.select("tbody");
+
+//Loop Through `data` and console.log each ufo report object
+//Use `Object.entries` to console.log each ufo report value
+data.forEach(function(uforeport) {
+//console.log(uforeport);
+var row = tbody.append("tr");
+
+Object.entries(uforeport).forEach(function([key, value]) {
+//console.log(key, value);
+var cell = row.append("td");
+cell.text(value);
+});
+});
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+// Select the input element and get the raw HTML node
+var inputelement = d3.select("#datetime");
+
+// Get the value property of the input element
+var inputvalue = inputelement.property("value");
+
+console.log(inputvalue);
+//console.log(data);
+
+
+  
+// Clear table 
+tbody.html("");
+
+var filteredData = data.filter(bydate => bydate.datetime === inputvalue);
+console.log(filteredData);
+
+
+filteredData.forEach(function(uforeport) {
+    //console.log(uforeport);
+    var row = tbody.append("tr");
+    
+    Object.entries(uforeport).forEach(function([key, value]) {
+    //console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+    });
+    });
+});
+
